5add3d36173ffdf2edf69841f96f67fd410b4802
Jkeroro <104914788+Jkeroromk@users.noreply.github.com>
2025-01-24 17:17:30 -0500

    Update layout.tsx
    Fix the deployment problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Fredoka } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const fredoka = Fredoka({
   weight: ["400", "500", "600"],
@@ -20,10 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
+        <Script
           src="https://kit.fontawesome.com/7db96a5cb9.js"
           crossOrigin="anonymous"
-        ></script>
+        />
         <body className={`${fredoka.className} antialiased`}>{children}</body>
       </head>
     </html>
