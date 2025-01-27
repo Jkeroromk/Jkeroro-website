1a53d5c604829b8e310cdf4569dcde3e685c1aad
Jkeroro <104914788+Jkeroromk@users.noreply.github.com>
2025-01-27 10:31:21 -0500

    Update album.jsx

--- components/album.jsx
+++ components/album.jsx
@@ -1,33 +1,28 @@
-
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Album = () => {
-  return (
-    <div className='flex flex-col items-center justify-center mt-10 '>
-        <Image
-        src='/Room.jpg'
-        alt='album' 
-        width={550}
-        height={400}
-        className='rounded-2xl scale-90 sm:scale-100 sm:mb-5'
-        />
-        <Image
-        src='/lego-car.jpg'
-        alt='album'
-        width={550}
-        height={400}
-        className='rounded-2xl scale-90 sm:scale-100 sm:mb-5'
-        />
-        <Image
-        src='/coffee.jpg'
-        alt='album'
-        width={550}
-        height={400}
-        className='rounded-2xl scale-90 sm:scale-100 sm:mb-5'
-        />
-    </div>
-  )
-}
+  const images = [
+    { src: '/Room.jpg', alt: 'Room' },
+    { src: '/lego-car.jpg', alt: 'Lego Car' },
+    { src: '/coffee.jpg', alt: 'Coffee' },
+  ];
 
-export default Album
+  return (
+    <div className="flex flex-col items-center justify-center mt-10">
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image.src}
+          alt={image.alt}
+          width={550}
+          height={400}
+          className="rounded-2xl scale-90 sm:scale-100 sm:mb-5"
+          priority={index === 0} // Preload the first image
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Album;
